faf4879a9967f9ceebd55a961ba1c30938d114b4
Banee Ishaque K <baneeishaque@gmail.com>
2020-08-18 15:32:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import soupsieve as sv
+import re
+
+artist = 'AZ'
+artist_id = 14943
+search_term = 'i move like a'
+print(str(artist_id) + ' : ' + artist)
+print('Search for : ' + search_term)
+
+url = 'https://www.lyrics.com/artist.php?name=' + artist + '&aid=' + str(artist_id) + '&o=1'
+page_source = requests.get(url).text
+beautiful_soap_content = BeautifulSoup(page_source, "lxml")
+
+for song in sv.select('tr', sv.select_one('tbody', beautiful_soap_content)):
+    song_element = sv.select_one('a', song)
+    print('\n\nSong Title : ' + song_element.text)
+    song_url = 'https://www.lyrics.com' + song_element.get('href')
+    print('Song URL : ' + song_url + '\n')
+    song_page_source = requests.get(song_url).text
+    song_page_content = BeautifulSoup(song_page_source, "lxml")
+    # print('Song Lyrics')
+    song_lyrics = sv.select_one('pre', song_page_content).text
+    print(song_lyrics)
+
+    # if search_term in song_lyrics :
+    #     break
+
+    if re.search(search_term, song_lyrics, re.IGNORECASE):
+        print(search_term + ' Found On ' + song_element.text)
+        print('URL : ' + song_url)
+        break
